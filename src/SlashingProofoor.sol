17eb8438ffb59e6b8819f8aa25dbe81ffe0fc308
Toni Wahrstätter <51536394+nerolation@users.noreply.github.com>
2024-04-29 10:01:48 +0200

    Fix mix_in_length bug

--- src/SlashingProofoor.sol
+++ src/SlashingProofoor.sol
@@ -11,11 +11,13 @@ interface IBeaconRoots {
     function get(bytes32 timestamp) external view returns (bytes32);
 }
 
-// Contract for managing slashing proofs of validators
-contract SlashingProofoor is Merkleizer, MerkleProof {
+// Contract for managing slash proofs of validators
+contract FlashProofoor is Merkleizer, MerkleProof {
     uint public constant VALIDATOR_REGISTRY_LIMIT = 2**40;
     IBeaconRoots private beaconRootsContract = IBeaconRoots(0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02);
     uint256 private constant HISTORY_BUFFER_LENGTH = 8191;
+
+    event Debug(bytes32 _hash);
 
     constructor(bytes32[] memory _zerohashes) Merkleizer(_zerohashes) {}
 
@@ -28,42 +30,46 @@ contract SlashingProofoor is Merkleizer, MerkleProof {
     /**
      * @dev Verifies non-slashing proof of a validator using multiple Merkle proofs.
      * @param blockTimestamp The timestamp of the block related to the proof verification context.
-     * @param validatorsProof Merkle proofs for the beaconState.validators list.
-     * @param validatorsRoot The root hash of the beaconState.validators tree.
-     * @param validatorIndex Index of the validator in the beaconState.validators list.
      * @param validatorChunks Array of data chunks corresponding to the validator's attributes.
-     * @param proofBeaconState Merkle proof for the beacon state.
-     * @param rootBeaconState The beacon state root used for verification.
-     * @param proofBeaconBlock Merkle proof for the beacon block.
+     * @param validatorsProof Merkle proofs for the beaconState.validators list.
+     * @param validatorIndex Index of the validator in the beaconState.validators list.
+     * @param validatorsRoot The root hash of the beaconState.validators tree.
+     * @param beaconStateProof Merkle proof for the beacon state.
+     * @param nr_validators Number of validators in beacon state.
+     * @param beaconStateRoot The beacon state root used for verification.
+     * @param beaconBlockProof Merkle proof for the beacon block.
      * @return success True if all validations pass, otherwise reverts.
      */
     function verifyProof(
         uint256 blockTimestamp,
-        bytes32[] memory validatorsProof,
-        bytes32 validatorsRoot,
-        uint256 validatorIndex,
         bytes32[] memory validatorChunks,
-        bytes32[] memory proofBeaconState,
-        bytes32 rootBeaconState,
-        bytes32[] memory proofBeaconBlock
-    ) public view returns (bool success) {
+        bytes32[] memory validatorsProof,
+        uint256 validatorIndex,
+        bytes32 validatorsRoot,
+        uint256 nr_validators,
+        bytes32[] memory beaconStateProof,
+        bytes32 beaconStateRoot,
+        bytes32[] memory beaconBlockProof
+    ) public returns (bool success) {
         // Ensure the validator has not been slashed (the slashing flag in the chunk must be zero)
-        assert(validatorChunks[3] != bytes32(0));
+        require(validatorChunks[3] != bytes32(0), "Provided validator chunks indicate non-slashed validator");
 
         // Compute the hash tree root of the validator's chunks
         bytes32 valHashTreeRoot = merkleizeChunks(validatorChunks, 8);
+        emit Debug(valHashTreeRoot);
 
         // Verify the validator's position and inclusion in the state's validator list
         require(verify(validatorsProof, validatorsRoot, valHashTreeRoot, validatorIndex), "Validator proof failed");
 
         // Calculate the validators hash tree root by mixing in the number of validators
-        bytes32 stateValidatorsHashTreeRoot = mixInLength(validatorsRoot, validatorsProof.length);
+        bytes32 stateValidatorsHashTreeRoot = mixInLength(validatorsRoot, nr_validators);
+        emit Debug(stateValidatorsHashTreeRoot);
 
         // Verify the hash tree root of validators against the beacon state root
-        require(verify(proofBeaconState, rootBeaconState, stateValidatorsHashTreeRoot, 11), "BeaconState validation failed");
+        require(verify(beaconStateProof, beaconStateRoot, stateValidatorsHashTreeRoot, 11), "BeaconState validation failed");
 
         // Additional verification against the beacon block could be re-enabled if needed
-        require(verify(proofBeaconBlock, getRootFromTimestamp(blockTimestamp), rootBeaconState, 3));
+        require(verify(proofBeaconBlock, getRootFromTimestamp(blockTimestamp), beaconStateRoot, 3));
         return true;
     }
 }
